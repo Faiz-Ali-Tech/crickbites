3d693cd05f7186230a798758bf8b1c8c7d5e0f83
Faiz Ali <faiz@propulsiontechnologies.com>
2026-05-02 13:22:32 +0530

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,23 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+// import { drizzle } from "drizzle-orm/postgres-js";
+// import postgres from "postgres";
+// import * as schema from "@/db/schema";
+
+// const connectionString = process.env.DATABASE_URL!;
+// const client = postgres(connectionString, {
+//     ssl: "require",
+//     prepare: false,
+// });
+// export const db = drizzle(client, { schema });
+
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import * as schema from "@/db/schema";
 
-const connectionString = process.env.DATABASE_URL!;
-const client = postgres(connectionString, {
-    ssl: "require",
-    prepare: false,
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-export const db = drizzle(client, { schema });
+
+export const db = drizzle(pool, { schema });
